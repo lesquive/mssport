5437a5f62a857fbb6e832df79ff34a75c143fb6a
lesquive <luis.jose.esquivel@gmail.com>
2021-12-11 12:55:51 -0600

    Added Delete functionality, automatic price update, carrito count update & fixed bugs

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ const carritoItem = document.querySelector(".carritoItem");
 const carritoTotal = document.querySelector(".carritoTotal");
 const applicarDescuentoBttn = document.querySelector(".SMSBttn");
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 const closeModel = () => {
   console.log("cerrando welcome");
   welcome.classList.add("hidden");
@@ -52,16 +56,17 @@ const updateCarrito = (nombreProducto, precioProducto) => {
     }
     var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
     if (existingEntries == null) existingEntries = [];
-    var entry = { Producto: nombreProducto, Precio: precioProducto };
+    var id = getRandomInt(999999999);
+    var entry = { ID: id, Producto: nombreProducto, Precio: precioProducto };
     localStorage.total =
       parseInt(localStorage.total) + parseInt(precioProducto);
     localStorage.setItem("entry", JSON.stringify(entry));
     // Save allEntries back to local storage
     existingEntries.push(entry);
     localStorage.setItem("allEntries", JSON.stringify(existingEntries));
-    carritoItem.innerHTML += `<p>${nombreProducto} : ${precioProducto} <a href="javascript:void(0);"><img src="./imagenes/delete.png" alt="carrito" class="social-media"></a><p/>`;
+    carritoItem.innerHTML += `<p class=${id}>${nombreProducto} : ${precioProducto} <a href="javascript:void(0);"><img src="./imagenes/delete.png" alt="carrito" class="social-media deleteBttn ${id}" onclick="deleteItem(${id})"></a><p/>`;
     carritoTotal.innerHTML = `<p>Total: ${localStorage.total}$<p/>`;
-    console.log(localStorage.existingEntries);
+    console.log(localStorage.allEntries);
     console.log(localStorage.total);
   } else {
     // Sorry! No Web Storage support..
@@ -80,9 +85,9 @@ const checkCarrito = () => {
       var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
       if (existingEntries == null) existingEntries = [];
       for (let i = 0; i < existingEntries.length; i++) {
-        carritoItem.innerHTML += `<p>${existingEntries[i]["Producto"]} : ${existingEntries[i]["Precio"]} <a href="javascript:void(0);"><img src="./imagenes/delete.png" alt="carrito" class="social-media"></a><p/>`;
-        carritoTotal.innerHTML = `<p>Total: ${localStorage.total}$<p/>`;
+        carritoItem.innerHTML += `<p class=${existingEntries[i]["ID"]}>${existingEntries[i]["Producto"]} : ${existingEntries[i]["Precio"]} <a href="javascript:void(0);"><img src="./imagenes/delete.png" alt="carrito" class="social-media deleteBttn ${existingEntries[i]["ID"]}" onclick="deleteItem(${existingEntries[i]["ID"]})"></a><p/>`;
       }
+      carritoTotal.innerHTML = `<p>Total: ${localStorage.total}$<p/>`;
     } else {
       localStorage.carritoCount = 0;
       localStorage.total = 0;
@@ -91,6 +96,31 @@ const checkCarrito = () => {
   } else {
     // Sorry! No Web Storage support..
   }
+};
+
+const deleteItem = (id) => {
+  var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+  for (let i = 0; i < existingEntries.length; i++) {
+    if (existingEntries[i]["ID"] == id) {
+      localStorage.total =
+        localStorage.total - parseInt(existingEntries[i]["Precio"]);
+      existingEntries.splice(i, 1);
+      if (localStorage.carritoCount) {
+        localStorage.carritoCount = Number(localStorage.carritoCount) - 1;
+      } else {
+        localStorage.carritoCount = 0;
+      }
+    }
+  }
+  localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+  carritoItem.innerHTML = "";
+  for (let i = 0; i < existingEntries.length; i++) {
+    carritoItem.innerHTML += `<p class=${existingEntries[i]["ID"]}>${existingEntries[i]["Producto"]} : ${existingEntries[i]["Precio"]} <a href="javascript:void(0);"><img src="./imagenes/delete.png" alt="carrito" class="social-media deleteBttn ${existingEntries[i]["ID"]}" onclick="deleteItem(${existingEntries[i]["ID"]})"></a><p/>`;
+  }
+  carritoTotal.innerHTML = `<p>Total: ${localStorage.total}$<p/>`;
+  carritoLink.innerHTML =
+    '<img src="./imagenes/shopping-cart.png" alt="carrito" class="social-media" onclick="verCarritoFunc()">' +
+    localStorage.carritoCount;
 };
 
 const checkDiscount = () => {
